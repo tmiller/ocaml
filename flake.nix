bbd236b9c38cc1d1c16a4b331f3f71f951e13e36
Thomas Miller <git@me.tmiller.com>
2023-04-27 21:56:35 -0500

    Add everything

--- flake.nix
+++ flake.nix
@@ -8,9 +8,30 @@
     lib = pkgs.lib;
   in
   {
+    packages.${system} = {
+      default = self.packages.${system}.hello;
+
+      hello = with pkgs; stdenv.mkDerivation {
+        name = "hello";
+        version = "0.0.0";
+        src = ./.;
+        inherit ocaml;
+        buildPhase = ''
+          export PATH="$ocaml/bin:$PATH"
+          mkdir -p $out/bin
+          ocamlopt -o $out/bin/hello data.mli data.ml main.ml
+        '';
+      };
+    };
+    app.${system}.default = {
+      description = "A simple hello world application";
+      type = "service";
+      start = "${pkgs.hello}/bin/hello";
+    };
     devShells.${system}.default = pkgs.mkShell {
       packages = with pkgs; [
         ocaml
+        opam
         ocamlPackages.utop
       ];
     };
